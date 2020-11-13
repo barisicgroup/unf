a0ee6f73a0ca415c26ccf5d9961750d44b6442e9
mhaichao <mitharvard1987>
2020-11-13 13:25:04 +0100

    sample nucleotide entries for unf

--- version 0.1/loadfile.py
+++ version 0.1/loadfile.py
@@ -4,8 +4,7 @@ f = open('C:/Development/unf/version 0.1/loop.json',)
    
 data = json.load(f) 
 
-print(data['singleStrands']['1']['1'])
-
+print(data['singleStrands']['1'])
 
 # Closing file 
 f.close() 
