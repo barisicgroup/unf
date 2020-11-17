b03f5896f3dc106044ed54d58626e30586f1c0e2
davous267 <davedesign@email.cz>
2020-11-17 11:02:01 +0100

    Some helper funcs added to parser; parsed objects are now appended to one rescaled object having its local space transformed to have 1 unit == 1 angstrom

--- unf-web-viewer/js/unf_parser.js
+++ unf-web-viewer/js/unf_parser.js
@@ -2,54 +2,78 @@ import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
 
 var ParserConstants = {
     SupportedFormatVersion: .1,
+    AngstromsPerUnit: 256,
+    VHelixRadius: 20,
+    BasePairRise: 3.32,
+    RotationPerBp: 34.3,
     HoneycombGridName: "honeycomb",
     SquareGridName: "square",
-    VHelixRadius: .1,
-    BasePairRise: .017
 };
 
+var ParserUtils = {
+    angsToUnits(angstroms) {
+        return angstroms / ParserConstants.AngstromsPerUnit;
+    },
+
+    unitsToAngs(units) {
+        return units * ParserConstants.AngstromsPerUnit;
+    }
+}
+
 export function parseUNF(fileContent) {
-    let parsedJson = JSON.parse(fileContent);
+    const parsedJson = JSON.parse(fileContent);
     console.log(parsedJson); // Output to log for verification purposes
 
-    if(parsedJson.version !== ParserConstants.SupportedFormatVersion) {
+    if (parsedJson.version !== ParserConstants.SupportedFormatVersion) {
         alert("Unsupported format version!");
     }
 
     let result = [];
+
+    var rescaledParent = new THREE.Object3D();
+    rescaledParent.scale.set(
+        1 / ParserConstants.AngstromsPerUnit, 
+        1 / ParserConstants.AngstromsPerUnit, 
+        1 / ParserConstants.AngstromsPerUnit);
+    result.push(rescaledParent);
+
+    processVirtualHelices(parsedJson, rescaledParent);
+
+    return result;
+}
+
+function processVirtualHelices(parsedJson, objectsParent) {
     const geometry = new THREE.CylinderBufferGeometry(
         ParserConstants.VHelixRadius, ParserConstants.VHelixRadius, 1, 32, 32);
     const material = new THREE.MeshPhongMaterial({ color: 0xff4444 });
 
-    var vHelices = parsedJson.virtualHelices; // Not an array but an object!
+    var vHelices = parsedJson.virtualHelices;
     for (const [key, value] of Object.entries(vHelices)) {
         const newMesh = new THREE.Mesh(geometry, material);
         newMesh.position.copy(getPositionForIndex(value.gridPosition[0], value.gridPosition[1], parsedJson.grid));
         newMesh.position.z -= value.lastActiveCell * ParserConstants.BasePairRise * 0.5;
         newMesh.scale.set(1, value.lastActiveCell * ParserConstants.BasePairRise, 1);
         newMesh.rotation.set(THREE.MathUtils.degToRad(90), 0, 0);
-        result.push(newMesh);
+        objectsParent.add(newMesh);
     }
+   
+    function getPositionForIndex(x, y, gridType) {
+        const vHelixDiameter = ParserConstants.VHelixRadius * 2;
 
-    return result;
-}
+        if (gridType === ParserConstants.HoneycombGridName) {
+            return new THREE.Vector3(
+                x * ParserConstants.VHelixRadius * 1.7320508 + ParserConstants.VHelixRadius,
+                (y + Math.floor((y + 1 - x % 2) / 2)) * vHelixDiameter + vHelixDiameter * (0.5 + (x % 2) * 0.5),
+                0);
 
-function getPositionForIndex(x, y, gridType) {
-    // This is really ugly hardcoded stuff taken from Vivern
-    // but good enough for testing purposes.
-    if (gridType === ParserConstants.HoneycombGridName) {
-        return new THREE.Vector3(
-            x * ParserConstants.VHelixRadius * 2 * 0.8660254 + ParserConstants.VHelixRadius,
-            (y + Math.floor((y + 1 - x % 2) / 2)) * ParserConstants.VHelixRadius * 2 + ParserConstants.VHelixRadius * 2 * (0.5 + (x % 2) * 0.5),
-            0);
+        }
+        else if (ParserConstants.gridType === SquareGridName) {
+            return new THREE.Vector3(
+                x * vHelixDiameter + ParserConstants.VHelixRadius,
+                y * vHelixDiameter + ParserConstants.VHelixRadius,
+                0);
+        }
 
+        throw new Error("Invalid grid type!");
     }
-    else if (ParserConstants.gridType === SquareGridName) {
-        return new THREE.Vector3(
-            x * ParserConstants.VHelixRadius * 2 + ParserConstants.VHelixRadius,
-            y * ParserConstants.VHelixRadius * 2 + ParserConstants.VHelixRadius,
-            0);
-    }
-
-    throw new Error("Invalid grid type!");
 }
